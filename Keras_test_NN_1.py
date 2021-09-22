4b6451ccc104ed2533a51d1f935fd4dff6f3826c
Josh Dalin <jdalin3@gatech.edu>
2021-09-22 14:26:02 +0300

    Plotting

--- Keras_test_NN_1.py
+++ Keras_test_NN_1.py
@@ -1,6 +1,9 @@
 # Based on:
 # https://ai-pool.com/d/keras-get-layer-of-the-model
 # https://www.kaggle.com/shanekonaung/reuters-dataset-using-keras
+# https://stackoverflow.com/questions/43715047/how-do-i-get-the-weights-of-a-layer-in-keras
+# https://stackoverflow.com/questions/46817085/keras-interpreting-the-output-of-get-weights
+# https://stackoverflow.com/questions/42039548/how-to-check-the-weights-after-every-epoc-in-keras-model
 
 import tensorflow as tf
 import keras
@@ -9,6 +12,7 @@ from keras.layers import Dense
 from keras.datasets import reuters
 # built in keras function for one-hot encoding
 from keras.utils.np_utils import to_categorical
+from keras.callbacks import LambdaCallback
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -26,17 +30,6 @@ def to_one_hot(labels, dimension=46):
     for i,label in enumerate(labels):
         results[i,label] = 1
     return results
-
-model = Sequential()
-model.add(Dense(128, input_dim=784, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
 
 (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
@@ -56,35 +49,73 @@ one_hot_test_labels = to_categorical(test_labels)
 model = Sequential()
 model.add(Dense(64, activation='relu', input_shape=(10000,)))
 model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(46, activation='softmax'))
 
+weights = []
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-x_val = x_train[:1000]
+x_val = x_train[:100000]
 partial_x_train = x_train[1000:]
 
-y_val = one_hot_train_labels[:1000]
+y_val = one_hot_train_labels[:100000]
 partial_y_train = one_hot_train_labels[1000:]
 
+get_weights = LambdaCallback(on_batch_end=lambda batch, logs: weights.append(model.get_weights()))
 history = model.fit(partial_x_train,
                     partial_y_train,
-                    epochs=20,
-                    batch_size=512,
-                    validation_data=(x_val,y_val))
+                    epochs=1,
+                    batch_size=300,
+                    validation_data=(x_val,y_val),
+                    callbacks=[get_weights])
 
-print(history)
+first_layer_avg_weights = []
+first_layer_avg_biases = []
+seventh_layer_avg_weights = []
+seventh_layer_avg_biases = []
+for i in range(len(weights)):
+    epoch = weights[i]
+    first_layer_avg_weights.append(np.average(epoch[0]))
+    first_layer_avg_biases.append(np.average(epoch[1]))
+    seventh_layer_avg_weights.append(np.average(epoch[13]))
+    seventh_layer_avg_biases.append(np.average(epoch[14]))
+
+kernel = [-1, 1]
+first_layer_avg_biases = np.abs(np.convolve(first_layer_avg_biases, kernel))
+seventh_layer_avg_biases = np.abs(np.convolve(seventh_layer_avg_biases, kernel))
+first_layer_avg_weights = np.abs(np.convolve(first_layer_avg_weights, kernel))
+seventh_layer_avg_weights = np.abs(np.convolve(seventh_layer_avg_weights, kernel))
+print(first_layer_avg_biases)
+print(seventh_layer_avg_biases)
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 epochs = range(1, len(loss) + 1)
 
-plt.plot(epochs, loss, 'r', label='Training Loss')
-plt.plot(epochs, val_loss, 'b', label='Validation Loss')
-plt.title('Training Loss vs Validation Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
+# plt.plot(epochs, loss, 'r', label='Training Loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+# plt.plot(epochs, first_layer_avg_biases[:-1], label='first layer change in biases')
+# plt.plot(epochs, seventh_layer_avg_biases[:-1], label='seventh layer change in biases')
+x = []
+for i in range(len(first_layer_avg_weights)-1):
+    x.append(i+1)
+plt.subplot(2, 1, 1)
+plt.plot(x, first_layer_avg_biases[:-1], label='1st layer, delta biases')
+plt.plot(x, seventh_layer_avg_biases[:-1], label='7th layer, delta biases')
+
+plt.subplot(2, 1, 2)
+plt.plot(x, first_layer_avg_weights[:-1], label='1st layer, delta weight')
+plt.plot(x, seventh_layer_avg_weights[:-1], label='7th layer, delta weight')
+# plt.title('Change in Weights')
+plt.xlabel('Training Batches')
+plt.ylabel('Change')
 plt.legend()
+plt.savefig('Change_in_weights.png')
 plt.show()
