da9da31e88f6cc203ba75605a48d029d991a3f64
Josh Dalin <jdalin3@gatech.edu>
2021-10-16 23:56:25 +0300

    Word list creator: can parse jokes into nouns and verbs, then output similarity dataframes

--- HumorClassifierNN/word_list_creator.py
+++ HumorClassifierNN/word_list_creator.py
@@ -1,24 +1,30 @@
+import nltk
 from nltk.corpus import wordnet as wn
+from nltk import sent_tokenize
+from nltk import word_tokenize
 import pandas as pd
-# import pyjokes
+import pyjokes
+import numpy as np
 
+joke = pyjokes.get_joke()
+print(joke)
+print('\n')
 
-def get_similarities():
-    word_list = []
-    count = 10
-    words = wn.words()
-    count_2 = 0
-    for word in words:
-        if count_2 < 100:
-            count_2 += 1
-        else:
-            syn = wn.synsets(word)[0]
-            if syn.pos() == 'n':
-                word_list.append(word)
-                count -= 1
-                if count == 0:
-                    break
+def parse_joke(joke):
+    nouns = []
+    verbs = []
+    for word in word_tokenize(joke):
+        synset = wn.synsets(word)
+        if len(synset) > 0:
+            for syn in synset:
+                if word not in nouns and syn.pos() == 'n':
+                    nouns.append(word)
+                if word not in verbs and syn.pos() == 'v':
+                    verbs.append(word)
+    return nouns, verbs, joke
 
+def get_similarities(words = None):
+    word_list = words
     path_similarity = pd.DataFrame(index=word_list, columns=word_list)
     wup_similarity = pd.DataFrame(index=word_list, columns=word_list)
     lch_similarity = pd.DataFrame(index=word_list, columns=word_list)
@@ -30,3 +36,13 @@ def get_similarities():
             wup_similarity.iloc[i].iloc[j] = row_synset[0].wup_similarity(column_synset[0])
             lch_similarity.iloc[i].iloc[j] = row_synset[0].lch_similarity(column_synset[0])
     return path_similarity, wup_similarity, lch_similarity
+
+words = parse_joke(joke)
+similarities_nouns = get_similarities(words[0])
+
+print('path similarity:')
+print(similarities_nouns[0])
+print('\nwup similarity:')
+print(similarities_nouns[1])
+print('\nlch similarity:')
+print(similarities_nouns[2])
