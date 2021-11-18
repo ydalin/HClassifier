72bc2d6cea5a717415ea91b8175e3aa30b5b4f26
Thomas Dahlstrom <tdahlstrom3@gatech.edu>
2021-11-17 17:14:30 -0800

    CSS fix

--- final/OnePageMLApp-main/server.py
+++ final/OnePageMLApp-main/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from main import get_joke
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def index():
