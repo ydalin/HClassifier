df9d945ee3ed902f0a65652095313a4b46876435
ydalin <98948337+ydalin@users.noreply.github.com>
2022-02-07 18:07:39 +0200

    Finished cleaning up, added prompts, write to pickle option and generate directly option

--- HumorClassifierNN/ClassifierCode/main.py
+++ HumorClassifierNN/ClassifierCode/main.py
@@ -1,6 +1,5 @@
 from run_model import run_model
-from gather_data import gather_data
-
+from gather_data import write_to_pickle
 
 def main():
     """
@@ -8,15 +7,29 @@ def main():
     """
     prompt = "How to run? Input 1 or 2:" + \
              "\n1 Directly (very slow)" + \
-             "\n2 From saved 'stats.pkl' file (not as slow)"
-    directly = int(input(prompt)) == 1
-    if directly:
-        print("Running Directly")
-        data = gather_data()
-    else:
-        print("Running from saved stats.pkl file")
+             "\n2 From saved 'stats.pkl' file (not as slow)" + \
+             "\n3 Write data to 'stats.pkl' file"
+    action = int(input(prompt))
 
-    run_model()
+    if action not in [1, 2, 3]:
+        raise Exception("Please input 1 or 2 or 3")
+
+    if action == 1:
+        print("Running Directly")
+        run_model(directly=True)
+    elif action == 2:
+        print("Running from saved stats.pkl file")
+        run_model(directly=False)
+    else:
+        prompt = 'What test/train split to use (input a float)?'
+        split = input(prompt)
+        try:
+            split = float(split)
+        except ValueError:
+            print("Please enter a float.")
+        if split <= 0 or split >= 1:
+            raise Exception("Please enter a float between 0 and 1")
+        write_to_pickle(split=split)
 
 
 if __name__ == "__main__":
